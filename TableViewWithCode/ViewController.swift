675519a6bdbab8ffcee0e0520178e20b4de81cfa
Artem <inste@icloud.com>
2019-05-17 19:25:04 +0300

    Initial Commit

---
+++ TableViewWithCode/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableViewWithCode
+//
+//  Created by Артём Кармазь on 5/17/19.
+//  Copyright © 2019 Artem Karmaz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
